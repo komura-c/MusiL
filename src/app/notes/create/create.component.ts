5ad419e25f008cff7ebc9f76895580bdeac81669
komura <affirman03139@outlook.jp>
2020-06-02 15:03:21 +0900

    feat(create): 投稿フォームの作成

--- src/app/notes/create/create.component.ts
+++ src/app/notes/create/create.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+  styleUrls: ['./create.component.scss'],
 })
 export class CreateComponent implements OnInit {
+  form = this.fb.group({
+    title: ['', [Validators.required, Validators.maxLength(255)]],
+    tag: ['', Validators.required],
+    text: ['', Validators.required],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
