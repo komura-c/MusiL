fce1ae8bf9acd85e32a0c802db48efc8db17a1a0
komura <affirman03139@outlook.jp>
2020-06-06 16:21:50 +0900

    feat(create): 投稿フォームのバリデーション

--- src/app/notes/create/create.component.ts
+++ src/app/notes/create/create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
@@ -8,10 +8,18 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class CreateComponent implements OnInit {
   form = this.fb.group({
-    title: ['', [Validators.required, Validators.maxLength(255)]],
-    tag: ['', Validators.required],
+    title: ['', [Validators.required, Validators.maxLength(80)]],
+    tag: [''],
     text: ['', Validators.required],
   });
+
+  get titleControl() {
+    return this.form.get('title') as FormControl;
+  }
+
+  get textControl() {
+    return this.form.get('text') as FormControl;
+  }
 
   constructor(private fb: FormBuilder) {}
 
