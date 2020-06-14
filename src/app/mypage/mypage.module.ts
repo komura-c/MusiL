93babd9a9f025f0f0c20650f1d901bd2c47bf4f8
komura <affirman03139@outlook.jp>
2020-06-14 15:36:47 +0900

    feat(note): 記事詳細画面ルーティングとマークアップ

--- src/app/mypage/mypage.module.ts
+++ src/app/mypage/mypage.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { MypageRoutingModule } from './mypage-routing.module';
 import { MypageComponent } from './mypage/mypage.component';
-
+import { NoteComponent } from './note/note.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [MypageComponent],
-  imports: [
-    CommonModule,
-    MypageRoutingModule
-  ]
+  declarations: [MypageComponent, NoteComponent],
+  imports: [CommonModule, MypageRoutingModule, MatButtonModule, MatTooltipModule,
+    MatIconModule],
 })
 export class MypageModule { }
