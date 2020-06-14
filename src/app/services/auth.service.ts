f6fe3c21c74fb5c6eac1529110a8f578d21f46ef
komura <affirman03139@outlook.jp>
2020-06-15 02:50:50 +0900

    feat(auth): ログイン処理の実装

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth, User } from 'firebase/app';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor() { }
+  afUser$: Observable<User> = this.afAuth.user;
 
+  constructor(
+    private afAuth: AngularFireAuth
+  ) {
+  }
   login() {
-
+    this.afAuth.signInWithPopup(
+      new auth.TwitterAuthProvider()
+    );
   }
 
   logout() {
