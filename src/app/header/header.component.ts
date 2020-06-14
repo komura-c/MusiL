3446a95e85626c27517e808f1739e17f7b7158a6
komura <affirman03139@outlook.jp>
2020-06-15 02:50:50 +0900

    feat(auth): ログイン処理の実装

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  login() {
+    this.authService.login();
+  }
 }
