c836599f19e0384d28cfdfffa5d5b6e3842fba31
komura <affirman03139@outlook.jp>
2020-06-16 23:13:03 +0900

    feat(header): ログイン中のUIとマークアップ

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -8,9 +9,15 @@ import { AuthService } from '../services/auth.service';
 })
 export class HeaderComponent implements OnInit {
   user$ = this.authService.afUser$;
+  isProcessing = true;
+
   constructor(
     private authService: AuthService
-  ) { }
+  ) {
+    this.user$.pipe(take(1)).subscribe(() => {
+      this.isProcessing = false;
+    });
+  }
 
   ngOnInit(): void {
   }
