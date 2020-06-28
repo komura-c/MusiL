60ca5ba138471e9a1a80079c350379a0dbbb2d87
komura <affirman03139@outlook.jp>
2020-06-28 22:21:50 +0900

    feat(mypage): マイページのルートパラメーターを取得し、ユーザーデータを表示

--- src/app/mypage/mypage/mypage.component.ts
+++ src/app/mypage/mypage/mypage.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+import { Observable } from 'rxjs';
+import { UserData } from 'src/app/interfaces/user';
 
 @Component({
   selector: 'app-mypage',
@@ -6,8 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mypage.component.scss']
 })
 export class MypageComponent implements OnInit {
+  user$: Observable<UserData>;
+  userId: string;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService,
+  ) {
+    route.paramMap.subscribe(params => {
+      this.userId = params.get('id');
+      this.user$ = this.userService.getUserByScreenName(
+        this.userId
+      );
+    });
+  }
 
   ngOnInit(): void {
   }
