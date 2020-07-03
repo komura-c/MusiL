64d8b3b52531521174a1fa67b999623ead3e5fc8
komura <affirman03139@outlook.jp>
2020-07-03 16:04:50 +0900

    feat(home):getArticlesWithAuthorsの実装と記事のマークアップ

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Article } from 'src/app/interfaces/article';
+import { Article, ArticleWithAuthor } from 'src/app/interfaces/article';
+import { UserData } from 'src/app/interfaces/user';
 import { ArticleService } from 'src/app/services/article.service';
-import { Observable } from 'rxjs';
+import { Observable, of, combineLatest } from 'rxjs';
+import { switchMap, map } from 'rxjs/operators';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -9,9 +12,42 @@ import { Observable } from 'rxjs';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  articles$: Observable<Article[]> = this.articleService.getAllArticles();
+  articles$: Observable<ArticleWithAuthor[]> = this.getArticlesWithAuthors();
+
   constructor(
     private articleService: ArticleService,
+    private userService: UserService,
   ) { }
+
+  getArticlesWithAuthors(): Observable<ArticleWithAuthor[]> {
+    let articles: Article[];
+
+    return this.articleService.getAllArticles().pipe(
+      switchMap((docs: Article[]) => {
+        articles = docs;
+
+        if (articles.length) {
+          const authorIds: string[] = articles.filter((article, index, self) => {
+            return self.findIndex(item => article.uId === item.uId) === index;
+          }).map(post => post.uId);
+          return combineLatest(authorIds.map(uId => {
+            return this.userService.getUserByUId(uId);
+          }));
+        } else {
+          return of([]);
+        }
+      }),
+      map((users: UserData[]) => {
+        return articles.map(article => {
+          const result: ArticleWithAuthor = {
+            ...article,
+            author: users.find(user => user.uId === article.uId),
+          };
+          return result;
+        });
+      })
+    );
+  }
+
   ngOnInit(): void { }
 }
