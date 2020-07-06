5951689420ba006bb42f2014750799fc6b7a8818
komura <affirman03139@outlook.jp>
2020-07-06 12:04:16 +0900

    feat(about): サービス概要ページのマークアップ

--- src/app/about/about/about.component.ts
+++ src/app/about/about/about.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  isProcessing: boolean;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+    private router: Router,
+    private snackBar: MatSnackBar,
+  ) { }
 
   ngOnInit(): void {
   }
 
+  login() {
+    if (!this.authService.user$) {
+      this.isProcessing = true;
+      this.authService.login().finally(() => {
+        this.isProcessing = false;
+        this.userService.getUserByUId(this.authService.uId);
+      });
+    } else {
+      this.router.navigateByUrl('/');
+      this.snackBar.open('すでにログインしています', null, { duration: 2000 });
+    }
+  }
 }
