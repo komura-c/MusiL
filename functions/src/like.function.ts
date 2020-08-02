1f099288b1e291a4a0f19f1ffc30f8b2e1c44b24
komura <affirman03139@outlook.jp>
2020-08-03 07:19:18 +0900

    feat(function): like.function.tsの作成

---
+++ functions/src/like.function.ts
@@ -0,0 +1,40 @@
+
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import { shouldEventRun, markEventTried } from './utils/should.function';
+
+const db = admin.firestore();
+
+export const countUpLiked = functions
+  .region('asia-northeast1')
+  .firestore.document('users/{userId}/likedArticles/{articleId}')
+  .onCreate(async (snap, context) => {
+    const eventId = context.eventId;
+    return shouldEventRun(eventId).then(async (should: boolean) => {
+      if (should) {
+        await db
+          .doc(`articles/${context.params.articleId}`)
+          .update('likeCount', admin.firestore.FieldValue.increment(1));
+        return markEventTried(eventId);
+      } else {
+        return;
+      }
+    });
+  });
+
+export const countDownLiked = functions
+  .region('asia-northeast1')
+  .firestore.document('users/{userId}/likedArticles/{articleId}')
+  .onDelete(async (snap, context) => {
+    const eventId = context.eventId;
+    return shouldEventRun(eventId).then(async (should: boolean) => {
+      if (should) {
+        await db
+          .doc(`articles/${context.params.articleId}`)
+          .update('likeCount', admin.firestore.FieldValue.increment(-1));
+        return markEventTried(eventId);
+      } else {
+        return;
+      }
+    });
+  });
