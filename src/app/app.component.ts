155e946a78d9769445d3565aa82e45711b9eceb8
komura <affirman03139@outlook.jp>
2020-08-07 10:35:51 +0900

    feat(routing): refactor, showHeader追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,26 @@ import { LoadingService } from './services/loading.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'dtmplace';
-  activatedRouteData: Observable<Data>;
+  showHeader: boolean;
+  showFooter: boolean;
 
   loading$ = this.loadingService.loading$;
 
   constructor(
     private router: Router,
-    private activatedRoute: ActivatedRoute,
-    private loadingService: LoadingService,
+    private route: ActivatedRoute,
+    private loadingService: LoadingService
   ) {
-    this.router.events.forEach(event => {
-      if ((event instanceof NavigationEnd)) {
-        this.activatedRouteData = this.activatedRoute.firstChild.data;
+    this.router.events.forEach((event) => {
+      if (event instanceof NavigationEnd) {
+        this.showHeader =
+          this.route.snapshot.firstChild.data.showHeader !== false;
+        this.showFooter =
+          this.route.snapshot.firstChild.data.showFooter !== false;
       }
     });
   }
