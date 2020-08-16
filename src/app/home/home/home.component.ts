4ef6fe8173ea30bb75d7580ccd54788490dca725
komura <affirman03139@outlook.jp>
2020-08-16 21:06:32 +0900

    feat(home): トップページのスタイル調整、Welcomeを刷新

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -13,8 +13,11 @@ import { UserData } from '@interfaces/user';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  isLoading: boolean;
   isProcessing: boolean;
-  user$: Observable<UserData> = this.authService.user$;
+  user$: Observable<UserData> = this.authService.user$.pipe(
+    tap(() => this.isLoading = false)
+  );
 
   popularArticles$: Observable<
     ArticleWithAuthor[]
@@ -44,6 +47,7 @@ export class HomeComponent implements OnInit {
     private authService: AuthService
   ) {
     this.loadingService.toggleLoading(true);
+    this.isLoading = true;
   }
 
   login() {
@@ -53,5 +57,5 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 }
