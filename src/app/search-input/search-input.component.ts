b705244d5b644eee684c250c6ffec859e4a7cc5b
komura <affirman03139@outlook.jp>
2020-08-21 03:14:51 +0900

    fix(refactor): search-input, resultをリファクタ

--- src/app/search-input/search-input.component.ts
+++ src/app/search-input/search-input.component.ts
@@ -1,29 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormControl } from '@angular/forms';
 import { SearchService } from '../services/search.service';
 import { startWith, debounceTime } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-search-input',
   templateUrl: './search-input.component.html',
   styleUrls: ['./search-input.component.scss'],
 })
-export class SearchInputComponent implements OnInit {
-  searchControl: FormControl = new FormControl();
+export class SearchInputComponent implements OnInit, OnDestroy {
+  private subscription: Subscription;
+  private index = this.searchService.index.popular;
+  private searchOptions = {
+    page: 0,
+    hitsPerPage: 20,
+    facetFilters: ['isPublic:true'],
+  };
 
-  index = this.searchService.index.popular;
+  searchControl: FormControl = new FormControl();
   searchResult: {
     nbHits: number;
     hits: any[];
   };
-  searchOptions = {
-    page: 0,
-    hitsPerPage: 8,
-    facetFilters: ['isPublic:true'],
-  };
-
-  isSearchActive: boolean;
+  isSearchActive = false;
 
   constructor(
     private router: Router,
@@ -48,23 +49,17 @@ export class SearchInputComponent implements OnInit {
     });
   }
 
-  searchActive() {
-    if (this.isSearchActive === false) {
-      this.isSearchActive = true;
-    } else {
-      this.isSearchActive = false;
-    }
-  }
-
   ngOnInit(): void {
-    this.isSearchActive = false;
-    this.searchControl.valueChanges
-      .pipe(startWith(''), debounceTime(500))
-      .subscribe((keyword) => {
-        const searchKeyword: string = keyword;
+    this.subscription = this.searchControl.valueChanges.pipe(
+      startWith(''), debounceTime(500))
+      .subscribe((keyword: string) => {
         this.index
-          .search(searchKeyword, this.searchOptions)
+          .search(keyword, this.searchOptions)
           .then((searchResult) => (this.searchResult = searchResult));
       });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
