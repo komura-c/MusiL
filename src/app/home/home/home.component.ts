0232d66dd51422b3e1d9d9a9efeb4a785ec9cab1
komura <affirman03139@outlook.jp>
2020-08-20 19:25:17 +0900

    fix(refactor): headerとhomeページ、authをMTGにてリファクタ

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ArticleService } from 'src/app/services/article.service';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { ArticleWithAuthor } from 'functions/src/interfaces/article-with-author';
-import { tap, catchError } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
 import { LoadingService } from 'src/app/services/loading.service';
 import { AuthService } from 'src/app/services/auth.service';
 import { UserData } from '@interfaces/user';
@@ -14,7 +14,7 @@ import { ScrollService } from 'src/app/services/scroll.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  isLoading: boolean;
+  isLoading = true;
   isProcessing: boolean;
   user$: Observable<UserData> = this.authService.user$.pipe(
     tap(() => (this.isLoading = false))
@@ -26,11 +26,6 @@ export class HomeComponent implements OnInit, OnDestroy {
     tap(() => {
       this.loadingService.toggleLoading(false);
       this.scrollService.restoreScrollPosition('top-page');
-    }),
-    catchError((error) => {
-      console.log(error.message);
-      this.loadingService.toggleLoading(false);
-      return of(null);
     })
   );
 
@@ -38,11 +33,6 @@ export class HomeComponent implements OnInit, OnDestroy {
     ArticleWithAuthor[]
   > = this.articleService.getLatestArticles().pipe(
     tap(() => this.loadingService.toggleLoading(false)),
-    catchError((error) => {
-      console.log(error.message);
-      this.loadingService.toggleLoading(false);
-      return of(null);
-    })
   );
 
   constructor(
@@ -52,7 +42,6 @@ export class HomeComponent implements OnInit, OnDestroy {
     private scrollService: ScrollService
   ) {
     this.loadingService.toggleLoading(true);
-    this.isLoading = true;
   }
 
   login() {
