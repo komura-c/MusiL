7fdada9624d1eb9aed6c8b0439af79310caa8d73
komura <affirman03139@outlook.jp>
2020-08-23 04:14:51 +0900

    fix(functions): initializeをindex.tsに移動

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,6 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+admin.initializeApp(functions.config().firebase);
 export {
   createUser,
   deleteUserData,
