be72f9a9dd2da8f7deda1a2baf5012fde804d778
komura <affirman03139@outlook.jp>
2020-08-22 13:09:55 +0900

    fix(editor): エディターの処理を修正

--- src/app/notes/editor/editor.component.ts
+++ src/app/notes/editor/editor.component.ts
@@ -159,13 +159,8 @@ export class EditorComponent implements OnInit {
             soundCloudURL[2] +
             '&color=%23ff5500&auto_play=false&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>' +
             text;
-          const currentValue = this.parentForm.value;
-          this.parentForm.patchValue({
-            title: currentValue.title,
-            tags: currentValue.tags,
-            editorContent: currentValue.editorContent + soundCloudEmbedPlayer,
-            isPublic: currentValue.isPublic,
-          });
+          const currentValue = this.editorContentControl.value;
+          this.editorContentControl.patchValue(currentValue + soundCloudEmbedPlayer);
           this.ngZone.run(() => {
             const msg = 'SoundCloudの埋め込みが完了しました';
             this.snackBar.open(msg, '閉じる');
@@ -185,7 +180,7 @@ export class EditorComponent implements OnInit {
     private authService: AuthService,
     private articleService: ArticleService,
     private snackBar: MatSnackBar
-  ) {}
+  ) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 }
