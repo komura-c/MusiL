4ecf17c5178171205d079acaf74885b4bcc15e2d
komura <affirman03139@outlook.jp>
2020-08-23 00:47:32 +0900

    fix(seoservice): 例外処理を追加

--- src/app/services/seo.service.ts
+++ src/app/services/seo.service.ts
@@ -5,18 +5,26 @@ import { Meta, Title } from '@angular/platform-browser';
   providedIn: 'root'
 })
 export class SeoService {
+  notFoundMetas = {
+    title: `ページが見つかりません | MusiL`,
+    description: 'お探しのページは、削除されたかURLが変更された可能性があります。',
+    ogType: 'article',
+    ogImage: 'https://dtmplace-ad671.web.app/assets/images/ogp-cover.png',
+    twitterCard: 'summary_large_image',
+  };
+
   constructor(private meta: Meta, private title: Title) { }
 
   setTitleAndMeta(metaTags: { title: string, description: string, ogType: string, ogImage: string, twitterCard: string }) {
-    this.title.setTitle(metaTags.title);
+    this.title.setTitle(metaTags.title ? metaTags.title : this.notFoundMetas.title);
     this.meta.addTags([
-      { name: 'description', content: metaTags.description },
-      { property: 'og:title', content: metaTags.title },
-      { property: 'og:description', content: metaTags.description },
-      { property: 'og:type', content: metaTags.ogType },
+      { name: 'description', content: metaTags.description ? metaTags.title : this.notFoundMetas.description },
+      { property: 'og:title', content: metaTags.title ? metaTags.title : this.notFoundMetas.title },
+      { property: 'og:description', content: metaTags.description ? metaTags.title : this.notFoundMetas.description },
+      { property: 'og:type', content: metaTags.ogType ? metaTags.title : this.notFoundMetas.ogType },
       { property: 'og:url', content: location.href },
-      { property: 'og:image', content: metaTags.ogImage },
-      { name: 'twitter:card', content: metaTags.twitterCard },
+      { property: 'og:image', content: metaTags.ogImage ? metaTags.title : this.notFoundMetas.ogImage },
+      { name: 'twitter:card', content: metaTags.twitterCard ? metaTags.title : this.notFoundMetas.ogImage },
     ]);
   }
 }
