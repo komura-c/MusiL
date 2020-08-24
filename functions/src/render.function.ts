625b2f575d3b943481da28ed16bbfa71cf806f26
komura <affirman03139@outlook.jp>
2020-08-24 13:05:48 +0900

    feat(render): render.function追加リダイレクト、functionを最新に

--- functions/src/render.function.ts
+++ functions/src/render.function.ts
@@ -3,6 +3,7 @@ import * as express from 'express';
 import * as useragent from 'express-useragent';
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
+import xss from 'xss';
 import * as admin from 'firebase-admin'
 
 const db = admin.firestore();
@@ -11,33 +12,39 @@ const file = readFileSync(resolve(__dirname, '../lib/index.html'), {
   encoding: 'utf-8',
 });
 
-// 置換ヘルパー
-const replacer = (data: string) => {
-  return (match: string, content: string): string => {
-    return match.replace(content, data);
-  };
-};
-
 // 置換関数
-const buildHtml = (article: { [key: string]: string }, user: FirebaseFirestore.DocumentData | undefined) => {
-  const html2textReg = new RegExp(/<("[^"]*"|'[^']*'|[^'">])*>/g);
+const buildHtml = (articleAndScreenName: { [key: string]: string }) => {
+  const title = xss(articleAndScreenName.title);
+  const htmlToTextReg = new RegExp(/<("[^"]*"|'[^']*'|[^'">])*>/g);
+  const description = xss(articleAndScreenName.text?.replace(htmlToTextReg, '').substr(0, 200));
+  const ogURL = xss('https://dtmplace-ad671.web.app/' + articleAndScreenName.screenName + '/n/' + articleAndScreenName.articleId);
+  const ogImage = xss(articleAndScreenName.thumbnailURL ? articleAndScreenName.thumbnailURL : 'https://dtmplace-ad671.web.app/assets/images/ogp-cover.png');
   return file
-    // descriptionを記事本文の先頭200文字に置換
+    .replace(/\<title>.*<\/title>/g, '<title>' + title + '</title>')
     .replace(
-      /<meta name="description" content="(.+)" \/>/gm,
-      replacer(article.text?.replace(html2textReg, '').substr(0, 200))
+      /<meta name="description" content="[^>]*>/g,
+      '<meta name="description" content="' + description + '" />'
     )
-    // OGP画像を記事のサムネイルURLに置換
-    .replace(/content="(.+ogp-cover.png)"/gm, replacer(article.thumbnailURL ? article.thumbnailURL : 'https://dtmplace-ad671.web.app/assets/images/ogp-cover.png'))
-    // タイトルを記事タイトルに置換
-    .replace(/<title>(.+)<\/title>"/gm, replacer(article.title))
-    // OGタイトルを記事タイトルに置換
     .replace(
-      /<meta property="og:title" content="(.+)" \/>"/gm,
-      replacer(article.title)
+      /<meta property="og:title" content="[^>]*>/g,
+      '<meta property="og:title" content="' + title + '" />'
     )
-    // OG:URLを記事URLに置換
-    .replace(/<meta property="og:url" content="(.+)" \/>/g, replacer('https://dtmplace-ad671.web.app/' + user?.screenName + '/n/' + article.articleId));
+    .replace(
+      /<meta property="og:description" content="[^>]*>/g,
+      '<meta property="og:description" content="' + description + '" />'
+    )
+    .replace(
+      /<meta property="og:type" content="[^>]*>/g,
+      '<meta property="og:type" content="article" />'
+    )
+    .replace(
+      /<meta property="og:url" content="[^>]*>/g,
+      '<meta property="og:url" content="' + ogURL + '" />'
+    )
+    .replace(
+      /<meta property="og:image" content="[^>]*>/g,
+      '<meta property="og:image" content="' + ogImage + '" />'
+    );
 };
 
 // expressアプリ初期化
@@ -46,15 +53,18 @@ const app = express();
 // ユーザーエージェント判定ヘルパーを導入
 app.use(useragent.express());
 
-app.get(':id/n/:id', async (req: any, res: any) => {
+app.get('/:screenName/n/:articleId', async (req: any, res: any) => {
   // ロボットであれば置換結果を返却
   if (req.useragent.isBot) {
-    // https://xxx/:id/n/:id のようなURLを元に記事データをDBから取得
-    const article = (await db.doc(`articles/${req.query.id}`).get())?.data();
-    const user = (await db.doc(`users/${article?.uid}`).get())?.data();
+    // https://xxx/:screenName/n/:articleId のようなURLを元に記事データをDBから取得
+    const article = (await db.doc(`articles/${req.params.articleId}`).get())?.data();
     if (article) {
+      const articleAndScreenName = {
+        ...article,
+        screenName: req.params.screenName,
+      };
       // 結果を返却
-      res.send(buildHtml(article, user));
+      res.send(buildHtml(articleAndScreenName));
       return;
     }
   }
