d663b730bbeaba6739782fd6961b83af6f7494f2
komura <affirman03139@outlook.jp>
2020-08-24 22:21:29 +0900

    feat(ogp): OGP画像生成サービスを追加、renderのためのindex.html追加

--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -8,6 +8,7 @@ import { Article } from 'functions/src/interfaces/article';
 import { combineLatest, Observable, of } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
 import { UserService } from './user.service';
+import { OgpService } from './ogp.service';
 
 @Injectable({
   providedIn: 'root',
@@ -16,9 +17,58 @@ export class ArticleService {
   constructor(
     private db: AngularFirestore,
     private storage: AngularFireStorage,
-    private userService: UserService
+    private userService: UserService,
+    private ogpService: OgpService,
   ) { }
   snapArticleId: string;
+
+  async uploadImage(uid: string, file: File): Promise<void> {
+    const time: number = new Date().getTime();
+    const result = await this.storage
+      .ref(`users/${uid}/images/${time}`)
+      .put(file);
+    return await result.ref.getDownloadURL();
+  }
+
+  createArticle(
+    article: Omit<
+      Article,
+      'articleId' | 'createdAt' | 'updatedAt' | 'likeCount'
+    >
+  ): Promise<void> {
+    const articleId = this.db.createId();
+    this.snapArticleId = articleId;
+    const resultArticle = {
+      articleId,
+      ...article,
+      likeCount: 0,
+      createdAt: firestore.Timestamp.now(),
+      updatedAt: firestore.Timestamp.now(),
+    };
+    this.ogpService.createOgpImageAndUpload(resultArticle);
+    return this.db.doc(`articles/${articleId}`).set(resultArticle);
+  }
+
+  updateArticle(
+    articleId: string,
+    article: Omit<
+      Article,
+      'articleId' | 'createdAt' | 'updatedAt' | 'likeCount'
+    >
+  ): Promise<void> {
+    this.snapArticleId = articleId;
+    const resultArticle = {
+      articleId,
+      ...article,
+      updatedAt: firestore.Timestamp.now(),
+    };
+    this.ogpService.createOgpImageAndUpload(resultArticle);
+    return this.db.doc(`articles/${articleId}`).update(resultArticle);
+  }
+
+  deleteArticle(articleId: string): Promise<void> {
+    return this.db.doc(`articles/${articleId}`).delete();
+  }
 
   getMyArticlesPublic(uid: string): Observable<ArticleWithAuthor[]> {
     return this.db
@@ -80,49 +130,6 @@ export class ArticleService {
 
   getArticleOnly(articleId: string): Observable<Article> {
     return this.db.doc<Article>(`articles/${articleId}`).valueChanges();
-  }
-
-  async uploadImage(uid: string, file: File): Promise<void> {
-    const time: number = new Date().getTime();
-    const result = await this.storage
-      .ref(`users/${uid}/images/${time}`)
-      .put(file);
-    return await result.ref.getDownloadURL();
-  }
-
-  createArticle(
-    article: Omit<
-      Article,
-      'articleId' | 'createdAt' | 'updatedAt' | 'likeCount'
-    >
-  ): Promise<void> {
-    const articleId = this.db.createId();
-    this.snapArticleId = articleId;
-    return this.db.doc(`articles/${articleId}`).set({
-      articleId,
-      ...article,
-      likeCount: 0,
-      createdAt: firestore.Timestamp.now(),
-      updatedAt: firestore.Timestamp.now(),
-    });
-  }
-
-  updateArticle(
-    articleId: string,
-    article: Omit<
-      Article,
-      'articleId' | 'createdAt' | 'updatedAt' | 'likeCount'
-    >
-  ): Promise<void> {
-    this.snapArticleId = articleId;
-    return this.db.doc(`articles/${articleId}`).update({
-      ...article,
-      updatedAt: firestore.Timestamp.now(),
-    });
-  }
-
-  deleteArticle(articleId: string): Promise<void> {
-    return this.db.doc(`articles/${articleId}`).delete();
   }
 
   getPopularArticles(): Observable<ArticleWithAuthor[]> {
