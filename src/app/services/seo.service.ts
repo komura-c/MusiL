b2698105bc292d5ec14845214f760bcda01eed56
komura <affirman03139@outlook.jp>
2020-09-01 21:08:09 +0900

    fix(meta): メタタグの置換が誤っていたのを修正

--- src/app/services/seo.service.ts
+++ src/app/services/seo.service.ts
@@ -15,7 +15,7 @@ export class SeoService {
     twitterCard: 'summary_large_image',
   };
 
-  constructor(private meta: Meta, private title: Title) {}
+  constructor(private meta: Meta, private title: Title) { }
 
   setTitleAndMeta(metaTags: {
     title: string;
@@ -31,7 +31,7 @@ export class SeoService {
       {
         name: 'description',
         content: metaTags.description
-          ? metaTags.title
+          ? metaTags.description
           : this.notFoundMetas.description,
       },
       {
@@ -41,22 +41,22 @@ export class SeoService {
       {
         property: 'og:description',
         content: metaTags.description
-          ? metaTags.title
+          ? metaTags.description
           : this.notFoundMetas.description,
       },
       {
         property: 'og:type',
-        content: metaTags.ogType ? metaTags.title : this.notFoundMetas.ogType,
+        content: metaTags.ogType ? metaTags.ogType : this.notFoundMetas.ogType,
       },
       { property: 'og:url', content: location.href },
       {
         property: 'og:image',
-        content: metaTags.ogImage ? metaTags.title : this.notFoundMetas.ogImage,
+        content: metaTags.ogImage ? metaTags.ogImage : this.notFoundMetas.ogImage,
       },
       {
         name: 'twitter:card',
         content: metaTags.twitterCard
-          ? metaTags.title
+          ? metaTags.twitterCard
           : this.notFoundMetas.twitterCard,
       },
     ]);
