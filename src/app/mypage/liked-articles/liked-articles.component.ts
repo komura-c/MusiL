9703c0602caacb46bc15f5c17c010aaeca9eb791
komura <affirman03139@outlook.jp>
2020-09-09 01:18:38 +0900

    fix(service): サービスでデータを保持したが追いづらいため、引数で渡すようにリファクタ

--- src/app/mypage/liked-articles/liked-articles.component.ts
+++ src/app/mypage/liked-articles/liked-articles.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ArticleWithAuthor } from '@interfaces/article-with-author';
-import { Observable } from 'rxjs';
-import { tap, take } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { tap, take, map, switchMap } from 'rxjs/operators';
 import { ArticleService } from 'src/app/services/article.service';
+import { ActivatedRoute } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -11,23 +12,39 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./liked-articles.component.scss'],
 })
 export class LikedArticlesComponent implements OnInit {
+  screenName$: Observable<string> = this.route.parent.paramMap.pipe(
+    map((params) => params.get('id'))
+  );
+
+  private user$ = this.screenName$.pipe(
+    switchMap((screenName) => {
+      return this.userService.getUserByScreenName(screenName).pipe(take(1));
+    })
+  );
+
   articles$: Observable<
     ArticleWithAuthor[]
-  > = this.articleService
-    .getMyLikedArticles(this.userService.mypageUser?.uid)
-    .pipe(
-      take(1),
-      tap(() => {
-        this.isLoading = false;
-      })
-    );
+  > = this.user$.pipe(
+    switchMap((user) => {
+      if (user) {
+        return this.articleService.getMyLikedArticles(user.uid).pipe(take(1));
+      } else {
+        return of(null);
+      }
+    }),
+    take(1),
+    tap(() => {
+      this.isLoading = false;
+    })
+  );
 
   isLoading = true;
 
   constructor(
+    private route: ActivatedRoute,
+    private articleService: ArticleService,
     private userService: UserService,
-    private articleService: ArticleService
-  ) {}
+  ) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 }
