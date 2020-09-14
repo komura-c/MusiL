e9ea4c159372e5e166fb809580c456520aee5325
komura <affirman03139@outlook.jp>
2020-09-15 01:31:02 +0900

    feat(about): aboutページを変更、アイコンをsvgに変更

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -5,9 +5,17 @@ import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about/about.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [AboutComponent],
-  imports: [CommonModule, AboutRoutingModule, MatIconModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    AboutRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatCardModule],
 })
-export class AboutModule {}
+export class AboutModule { }
