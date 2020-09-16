36d7e60c1a8e9d16b8a3b3fc542a11454964f7c3
komura-c <komura-c@users.noreply.github.com>
2020-09-16 17:12:22 +0000

    Apply Prettier Change

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -15,7 +15,7 @@ export class HeaderComponent implements OnInit {
     this.isLoading = true;
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
   login() {
     this.authService.loginProcessing = true;
