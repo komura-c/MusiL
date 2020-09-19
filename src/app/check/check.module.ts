86fa8e9524044afd167d9d1636daa7bce916693a
komura <affirman03139@outlook.jp>
2020-09-19 23:00:06 +0900

    feat(check): checkの追加

---
+++ src/app/check/check.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CheckRoutingModule } from './check-routing.module';
+import { CheckComponent } from './check/check.component';
+
+
+@NgModule({
+  declarations: [CheckComponent],
+  imports: [
+    CommonModule,
+    CheckRoutingModule
+  ]
+})
+export class CheckModule { }
