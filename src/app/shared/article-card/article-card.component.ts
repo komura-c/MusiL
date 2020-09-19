08e9015000776c53d0f3c1cde1de7707dd55c46d
komura <affirman03139@outlook.jp>
2020-09-19 18:50:27 +0900

    fix(like): いいねの不具合を修正

--- src/app/shared/article-card/article-card.component.ts
+++ src/app/shared/article-card/article-card.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ArticleWithAuthor } from 'functions/src/interfaces/article-with-author';
+import { take } from 'rxjs/operators';
+import { AuthService } from 'src/app/services/auth.service';
+import { LikeService } from 'src/app/services/like.service';
 
 @Component({
   selector: 'app-article-card',
@@ -8,8 +11,28 @@ import { ArticleWithAuthor } from 'functions/src/interfaces/article-with-author'
 })
 export class ArticleCardComponent implements OnInit {
   @Input() article: ArticleWithAuthor;
+  isLiked: boolean;
+  likeCount: number;
 
-  constructor() {}
+  constructor(
+    private likeService: LikeService,
+    private authService: AuthService,
+  ) { }
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.likeCount = this.article.likeCount;
+    this.updateLikeCount();
+  }
+
+  updateLikeCount() {
+    if (this.authService.uid) {
+      this.likeService
+        .isLiked(this.article.articleId, this.authService.uid)
+        .pipe(take(1))
+        .toPromise().then((result) => {
+          this.isLiked = result;
+          this.likeCount = this.article.likeCount;
+        });
+    }
+  }
 }
