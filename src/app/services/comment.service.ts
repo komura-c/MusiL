c75e4cc0faed3cf477e4aa2da086b0eac4251ff6
komura <affirman03139@outlook.jp>
2020-11-04 00:19:29 +0900

    feat(comment.service): コメントサービスを作成

---
+++ src/app/services/comment.service.ts
@@ -0,0 +1,83 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ArticleComment } from '@interfaces/article-comment';
+import { ArticleCommentWithAuthor } from '@interfaces/article-comment-with-author';
+import { UserData } from '@interfaces/user';
+import { firestore } from 'firebase/app';
+import { combineLatest, Observable, of } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { UserService } from './user.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CommentService {
+  constructor(private db: AngularFirestore, private userService: UserService) {}
+
+  sendComment(articleId: string, text: string, user: UserData): Promise<void> {
+    const commentId = this.db.createId();
+    const newComment: ArticleComment = {
+      articleId,
+      commentId,
+      uid: user.uid,
+      text,
+      createdAt: firestore.Timestamp.now(),
+    };
+    return this.db
+      .doc<ArticleComment>(`articles/${articleId}/comments/${commentId}`)
+      .set(newComment);
+  }
+
+  deleteComment(articleId: string, commentId: string): Promise<void> {
+    return this.db
+      .doc<ArticleComment>(`articles/${articleId}/comments/${commentId}`)
+      .delete();
+  }
+
+  getLatestArticleComments(
+    articleId: string
+  ): Observable<ArticleCommentWithAuthor[]> {
+    const sorted: Observable<ArticleComment[]> = this.db
+      .doc(`articles/${articleId}`)
+      .collection<ArticleComment>('comments', (ref) =>
+        ref.orderBy('createdAt', 'desc').limit(30)
+      )
+      .valueChanges();
+    return this.getArticleCommentsWithAuthors(sorted);
+  }
+
+  getArticleCommentsWithAuthors(
+    sorted: Observable<ArticleComment[]>
+  ): Observable<ArticleCommentWithAuthor[]> {
+    let comments: ArticleComment[];
+    return sorted.pipe(
+      switchMap((commentsArray: ArticleComment[]) => {
+        if (commentsArray?.length) {
+          comments = commentsArray;
+          const authorIds: string[] = commentsArray.map((post) => post.uid);
+          const authorUniqueIds: string[] = Array.from(new Set(authorIds));
+          return combineLatest(
+            authorUniqueIds.map((userId) => {
+              return this.userService.getUserData(userId);
+            })
+          );
+        } else {
+          return of([]);
+        }
+      }),
+      map((users: UserData[]) => {
+        if (comments?.length) {
+          return comments.map((comment: ArticleComment) => {
+            const result: ArticleCommentWithAuthor = {
+              ...comment,
+              author: users?.find((user: UserData) => user.uid === comment.uid),
+            };
+            return result;
+          });
+        } else {
+          return null;
+        }
+      })
+    );
+  }
+}
