c49e79b98ca5f6f2ef68ee8ff86c5927ff1e9638
komura <affirman03139@outlook.jp>
2021-01-14 21:06:53 +0900

    fix(article-random): 不要になったinitを削除

--- functions/src/article-random.function.ts
+++ functions/src/article-random.function.ts
@@ -1,47 +1,8 @@
-import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import { DocumentData } from '@google-cloud/firestore';
 import { ArticleRandom } from './interfaces/article-random';
 
-const config = functions.config();
 const db = admin.firestore();
-
-export const initRandomArticle = functions
-  .region('asia-northeast1')
-  .https.onRequest(async (req: any, res: any) => {
-    if (req.query.access_token_key === config.twitter_bot.access_token_key) {
-      const resultData = await initArticleRandomAndTotalArticleCount();
-      return res.status(200).send(resultData);
-    }
-    res.status(404).send("Forbidden you don't have permission");
-  });
-
-async function initArticleRandomAndTotalArticleCount(): Promise<boolean> {
-  const articlesQuerySnapshot = await db.collection(`articles`).get();
-  if (articlesQuerySnapshot) {
-    const totalArticleCount = articlesQuerySnapshot.size;
-    await db.doc(`appMeta/totalArticleCount`).set({ totalArticleCount });
-    const batch = db.batch();
-    const articles: DocumentData[] = [];
-    articlesQuerySnapshot.forEach((articleQuerySnapshot) => {
-      articles.push(articleQuerySnapshot.data());
-    });
-    articles.forEach((article, index) => {
-      const articleRandomRef = db.doc(`articleRandom/${article.articleId}`);
-      const articleRandomDoc: ArticleRandom = {
-        articleId: article.articleId,
-        isPublic: article.isPublic,
-        randomNumber: index,
-        randomCheck: false,
-      };
-      batch.set(articleRandomRef, articleRandomDoc, { merge: true });
-    });
-    await batch.commit();
-    return true;
-  } else {
-    throw new Error('記事データの取得に失敗しました');
-  }
-}
 
 export async function getTotalArticleCount(): Promise<
   DocumentData | undefined
