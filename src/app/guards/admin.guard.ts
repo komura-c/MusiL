d00525bdc5655340871d221334c4ecdcce0a1ebb
komura <affirman03139@outlook.jp>
2021-01-14 23:03:25 +0900

    fix(admin): userClaimを実装

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -10,8 +10,7 @@ import {
   Router,
 } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map, take, tap } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
+import { switchMap, take, tap } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
@@ -29,7 +28,10 @@ export class AdminGuard implements CanActivate, CanLoad {
     | boolean
     | UrlTree {
     return this.authService.afUser$.pipe(
-      map((user) => !!(user?.uid === environment.hostUid)),
+      switchMap(async (user) => {
+        const idTokenResult = await user.getIdTokenResult();
+        return idTokenResult.claims.admin;
+      }),
       tap((isAdmin) => {
         if (!isAdmin) {
           this.router.navigateByUrl('/');
@@ -42,7 +44,10 @@ export class AdminGuard implements CanActivate, CanLoad {
     segments: UrlSegment[]
   ): Observable<boolean> | Promise<boolean> | boolean {
     return this.authService.afUser$.pipe(
-      map((user) => !!(user?.uid === environment.hostUid)),
+      switchMap(async (user) => {
+        const idTokenResult = await user.getIdTokenResult();
+        return idTokenResult.claims.admin;
+      }),
       take(1),
       tap((isAdmin) => {
         if (!isAdmin) {
