912fe800520c738406328bd4aa468a426be15025
komura <affirman03139@outlook.jp>
2021-03-07 05:44:24 +0900

    fix(app): prodで参照しないように修正

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,11 +21,7 @@ import {
 } from '@angular/fire/analytics';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
-import {
-  AngularFireFunctionsModule,
-  REGION,
-  USE_EMULATOR,
-} from '@angular/fire/functions';
+import { AngularFireFunctionsModule, REGION } from '@angular/fire/functions';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
 import { SearchInputComponent } from './search-input/search-input.component';
@@ -78,7 +74,7 @@ import { RouterModule } from '@angular/router';
     { provide: MatPaginatorIntl, useClass: MatPaginatorIntlJaModule },
     ScreenTrackingService,
     UserTrackingService,
-    { provide: USE_EMULATOR, useValue: ['localhost', 5001] },
+    // { provide: USE_EMULATOR, useValue: ['localhost', 5001] },
   ],
   bootstrap: [AppComponent],
 })
