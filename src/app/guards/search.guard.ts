155b6602bc09061f99a4cf9751a1949d64a3d6f1
komura <affirman03139@outlook.jp>
2021-08-30 04:25:42 +0900

    fix: fix typescript error

--- src/app/guards/search.guard.ts
+++ src/app/guards/search.guard.ts
@@ -2,30 +2,23 @@ import { Injectable } from '@angular/core';
 import {
   CanActivate,
   ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-  UrlTree,
   Router,
 } from '@angular/router';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SearchGuard implements CanActivate {
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ):
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree>
-    | boolean
-    | UrlTree {
+  ): boolean {
     if (next.queryParamMap?.get('q') || next.paramMap?.get('id')) {
       return true;
     } else {
       this.router.navigateByUrl('/');
+      return false;
     }
   }
 }
