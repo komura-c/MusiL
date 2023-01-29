7266610789068f13f92ba5ba63c899e29d60e07e
komura <affirman03139@outlook.jp>
2023-01-29 13:00:37 +0900

    feat: standalone attempt

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,36 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
+
+// TODO: 後に置き換え用
+// bootstrapApplication(AppComponent, {
+//   providers: [
+//     { provide: REGION, useValue: 'asia-northeast1' },
+//     { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { duration: 4000 } },
+//     { provide: MatPaginatorIntl, useClass: MatPaginatorIntlJaModule },
+//     ScreenTrackingService,
+//     UserTrackingService,
+//     // { provide: USE_EMULATOR, useValue: ['localhost', 5001] },
+//     importProvidersFrom([
+//       BrowserModule,
+//       AppRoutingModule,
+//       BrowserAnimationsModule,
+//       AngularFireModule.initializeApp(environment.firebase),
+//       AngularFireAnalyticsModule,
+//       AngularFirestoreModule,
+//       AngularFireStorageModule,
+//       AngularFireFunctionsModule,
+//       AngularFireAuthModule,
+//       MatSnackBarModule,
+//       MatListModule,
+//       FormsModule,
+//       ReactiveFormsModule,
+//       MatAutocompleteModule,
+//       MatProgressSpinnerModule,
+//     ]),
+//     provideHttpClient(),
+//   ],
+// }).catch((err) => console.error(err));
