66353a8732c6526f888a2d7e5b75e16dcb11d73b
komura <affirman03139@outlook.jp>
2023-06-19 01:04:05 +0900

    fix: budgets

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { MatLegacyMenuModule as MatMenuModule } from '@angular/material/legacy-menu';
 import { AuthServiceStub } from 'src/test/service.stub';
-import { AuthService } from '../services/auth.service';
+import { AuthService } from 'src/app/services/auth.service';
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
